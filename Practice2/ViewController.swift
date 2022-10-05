b7479376a23b12a12dfeb2eb883b1ae5890458a1
wave1321 <snowbovin@gmail.com>
2022-10-05 22:36:29 +0300

    #2

--- Practice2/ViewController.swift
+++ Practice2/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let picker = UIPickerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        picker.center = self.view.center
+        
+        picker.dataSource = self
+        picker.delegate = self
+        
+        self.view.addSubview(picker)
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UIPickerViewDataSource {
+    
+    //Сколько компонентов выводить
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Сколько всего компонентов в списке
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let result = "row = \(row)"
+        
+        return result
+    }
+    
+}
